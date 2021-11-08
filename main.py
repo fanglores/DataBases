6c81ff8e3be6bffdfb0c2bf8a073f902a91ecac8
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-11-08 14:47:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,44 +6,79 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
 #click functions
-def update_click():
+def insert_button_click():
+    print('Got click!')
+
+def update_button_click():
+    print('Got click!')
+
+def delete_button_click():
+    print('Got click!')
+
+def search_button_click():
     print('Got click!')
 
 
 
 
-
-
-
 #create connection
-db_con = pc2.connect(database='TelephonesArchive', user='postgres', password='1557', host='localhost', port=5432)
-cursor = db_con.cursor()
+try:
+    db_con = pc2.connect(database='TelephonesArchive', user='postgres', password='1557', host='localhost', port=5432)
+    cursor = db_con.cursor()
+except:
+    print('Error while connecting to a database')
+    exit(1)
 
 #create application
-app = QApplication(sys.argv)
+try:
+    app = QApplication(sys.argv)
+    
+    #create window
+    window = QWidget()
 
-#create window
-window = QWidget()
+    #window init
+    window.setWindowTitle('DataBase Interface v1.0')
+    window.setFixedSize(PyQt5.QtCore.QSize(720, 480))
 
-#window init
-window.setWindowTitle('DataBase Interface v1.0')
-window.setFixedSize(PyQt5.QtCore.QSize(720, 480))
+    insert_button = QPushButton(window)
+    insert_button.setText('INSERT')
+    insert_button.move(160, 200)
+    insert_button.clicked.connect(insert_button_click)
 
-button = QPushButton(window)
-button.setText('UPDATE')
-button.move(100, 100)
-button.clicked.connect(update_click)
+    update_button = QPushButton(window)
+    update_button.setText('UPDATE')
+    update_button.move(260, 200)
+    update_button.clicked.connect(update_button_click)
 
-window.layout()
+    delete_button = QPushButton(window)
+    delete_button.setText('DELETE')
+    delete_button.move(360, 200)
+    delete_button.clicked.connect(update_button_click)
 
-window.show()
+    search_button = QPushButton(window)
+    search_button.setText('SEARCH')
+    search_button.move(460, 200)
+    search_button.clicked.connect(update_button_click)
+except:
+    print('Error while initializing application')
+    app.closeAllWindows()
+    cursor.close()
+    db_con.close()
+    exit(1)
 
+#start the application
+try:
+    window.show()
+    app.exec()
+except:
+    print('Runtime error')
+    app.closeAllWindows()
+    cursor.close()
+    db_con.close()
+    exit(1)
 
-
-#start the event loop
-app.exec()
-
-#clear connection
+#clear all
+app.closeAllWindows()
 cursor.close()
 db_con.close()
-
+exit(0)
